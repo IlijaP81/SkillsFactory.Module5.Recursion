3a527da2e75d6f3cf8fa212f6adb38a2f6e8cb6b
IlijaP81 <niilp1999no@gmail.com>
2024-08-07 10:12:07 +0300

    Добавьте файлы проекта.

---
+++ Кecursion/Кecursion/Colors.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursion
+{
+        enum ColorsSet
+        {
+            Orange,
+            Blue,
+            Red,
+            Green,
+            Yellow,
+            Cyan,
+            Grey
+        }
+}
---
+++ Кecursion/Кecursion/Program.cs
@@ -0,0 +1,79 @@
+﻿
+using Recursion;
+
+
+class MainClass
+{
+    static void Main(string[] args)
+    {
+        string phrase; int deep;
+        phrase = DoCheckPhrase();
+        DoCheckDeep(out deep);
+        Echo(phrase, deep);
+
+        int powerUp = PowerUp(9, 3);
+        Console.WriteLine(powerUp.ToString());
+    }
+
+    private static string DoCheckPhrase()
+    {
+        Console.WriteLine("Введите фразу (более 2-х символов:");
+        string phrase = Console.ReadLine();
+        if (phrase.Length < 2)
+        {
+            Console.WriteLine("Фраза содержит менее 2-х символов");
+            DoCheckPhrase();
+        }
+        return phrase;
+    }
+
+    private static void DoCheckDeep(out int deep)
+    {
+        deep = 0;
+        int numberOfColors = Enum.GetNames(typeof(ColorsSet)).Length - 1;
+        Console.WriteLine("Определите глубину (целое число, не более " + numberOfColors);
+        try
+        {
+            deep = int.Parse(Console.ReadLine());
+            if (deep > numberOfColors) deep = numberOfColors;
+        }
+        catch
+        {
+            Console.WriteLine("Введите целое число не более " + numberOfColors);
+            DoCheckDeep(out deep);
+        }
+        return;
+    }
+
+    static void Echo(string message, int numOfIterations)
+    {
+        string messageCropped = message.Remove(0, 2);
+        Console.BackgroundColor = (ConsoleColor)(Enum.GetValues(typeof(ColorsSet)).GetValue(numOfIterations));
+        Console.WriteLine("..." + messageCropped);
+        if (messageCropped.Length > 2 & numOfIterations > 1)
+        {
+            numOfIterations--;
+            Echo(messageCropped, numOfIterations);
+        }
+        else return;
+    }
+
+    private static int PowerUp(int N, byte pow)
+    {
+        if (pow == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            if (pow == 1)
+            {
+                return N;
+            }
+            else
+            {
+                return N * PowerUp(N, --pow);
+            }
+        }
+    }
+}
